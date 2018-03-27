23d7c222ec58be2328052249836840dcf51a6b2c
weegeekelvin <weegeekelvin@gmail.com>
2018-03-27 07:33:39 +0100

    Setup Fragments
    - Setup fragments for the tabs
    - Removed example code
    - MainActivity now implements OnNavigationItemSelectedListener (Just to make the code cleaner)
    - Renamed tabs' ID to fit our use

--- app/src/main/java/com/example/alee7/soft254_weather_app/MainActivity.java
+++ app/src/main/java/com/example/alee7/soft254_weather_app/MainActivity.java
@@ -3,39 +3,55 @@ package com.example.alee7.soft254_weather_app;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_record);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_map);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private FrameLayout FrameMain;
+    private RecordFragment recordFragment;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        //Initialize FrameLayout
+        FrameMain = findViewById(R.id.FrameMain);
+
+        //Initialize fragments
+        recordFragment = new RecordFragment();
+        mapFragment = new MapFragment();
+
+        setFragment(recordFragment);    //Set the fragment to recordFragment when the app starts
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_record:
+                setFragment(recordFragment);
+                return true;
+            case R.id.navigation_map:
+                setFragment(mapFragment);
+                return true;
+        }
+        return false;
+    }
+
+    public void setFragment(Fragment fragment)
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.FrameMain, fragment);  //Assign fragment to the FrameLayout
+        transaction.commit();   //Apply changes
     }
 
 }
