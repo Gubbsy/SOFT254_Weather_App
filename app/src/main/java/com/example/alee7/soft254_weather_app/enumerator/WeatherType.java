2ac59ba013b20e0271f624782d8fc0e1e9710b72
weegeekelvin <weegeekelvin@gmail.com>
2018-04-03 13:37:31 +0100

    Created backend record classes and enums
    - Created backend record classes and enums

---
+++ app/src/main/java/com/example/alee7/soft254_weather_app/enumerator/WeatherType.java
@@ -0,0 +1,5 @@
+package com.example.alee7.soft254_weather_app.enumerator;
+
+public enum weatherType {
+
+}
---
+++ app/src/main/java/com/example/alee7/soft254_weather_app/enumerator/WindDirection.java
@@ -0,0 +1,4 @@
+package com.example.alee7.soft254_weather_app.enumerator;
+
+public enum WindDirection {
+}
