a664063f247bceac7390ba213c31bf05ae531fd1
Sour Games Dev <32270711+Gubbsy@users.noreply.github.com>
2018-04-16 13:28:47 +0100

    Create custom map markers form data pulled from fire base.

--- app/src/main/java/com/example/alee7/soft254_weather_app/frontend/MapFragment.java
+++ app/src/main/java/com/example/alee7/soft254_weather_app/frontend/MapFragment.java
@@ -1,14 +1,23 @@
 package com.example.alee7.soft254_weather_app.frontend;
 
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.alee7.soft254_weather_app.R;
+import com.example.alee7.soft254_weather_app.enumerator.WeatherType;
+import com.example.alee7.soft254_weather_app.enumerator.WindDirection;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -16,7 +25,17 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
 
 
 /**
@@ -27,19 +46,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
+    Context context;
+
+    FirebaseFirestore fbData = FirebaseFirestore.getInstance();
+    CollectionReference dbRef = fbData.collection("weather-info");
 
 
     public MapFragment() {
         // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        mView = inflater.inflate(R.layout.fragment_map, container, false);
-        return mView;
     }
 
     @Override
@@ -56,15 +70,84 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_map, container, false);
+        context = getActivity();
+        return mView;
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
-
         mGoogleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502)).title("Stature of de Liberty freedom").snippet("- Raining \n-37 Degrees"));
-        CameraPosition statLib = CameraPosition.builder().target(new LatLng(40.689247, -74.044502)).zoom(16).tilt(45).build();
+       dbRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+           @Override
+           public void onComplete(@NonNull Task<QuerySnapshot> task) {
+               if(task.isSuccessful()){
+                   for(QueryDocumentSnapshot document: task.getResult()){
+                       CreateMarker(googleMap,(HashMap<String, Object>) document.getData());
+                   }
+               }
+           }
+       });
+
+
+        CameraPosition statLib = CameraPosition.builder().target(new LatLng(50.689247, -4.044502)).zoom(16).tilt(45).build();
 
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(statLib));
+    }
+
+    public static int safeLongToInt(long l) {
+        if (l < Integer.MIN_VALUE || l > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException
+                    (l + " cannot be cast to int without changing its value.");
+        }
+        return (int) l;
+    }
+
+    public void CreateMarker(GoogleMap googleMap, HashMap<String,Object> recordItem) {
+
+        GeoPoint geoPoint = (GeoPoint) recordItem.get("location");
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude())).title("Stature of de Liberty freedom").snippet(
+                "Weather Type: " + WeatherType.getEnumByIndex(safeLongToInt((long)recordItem.get("weather-type"))).toString() +
+                        "\nFeels like: " + recordItem.get("user-temp") + "°C" +
+                        "\nWind Direction: " + WindDirection.getEnumByIndex(safeLongToInt((long)recordItem.get("wind-direction"))).toString() +
+                        "\nWind Speed: " + recordItem.get("wind-speed") + "mph" +
+                        "\nLocal Pressure: " + recordItem.get("pressure") +
+                        "\nRecorded Temp: " + recordItem.get("recorded-temp")));
+
+        googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                LinearLayout info = new LinearLayout(context);
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(context);
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(context);
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
     }
 }
