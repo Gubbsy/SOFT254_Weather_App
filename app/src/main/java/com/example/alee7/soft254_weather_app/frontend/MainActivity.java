d62bac0bc181beac2596111a3dc3c2022851701a
weegeekelvin <weegeekelvin@gmail.com>
2018-04-26 14:35:06 +0100

    Bug fixed
    - Fixed a bug where the app crashes when logging with empty email and passworc

--- app/src/main/java/com/example/alee7/soft254_weather_app/frontend/MainActivity.java
+++ app/src/main/java/com/example/alee7/soft254_weather_app/frontend/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -59,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this, getString(R.string.Please_Wait), getString(R.string.Processing), true);
 
-        (firebaseAuth.signInWithEmailAndPassword(textEmail.getText().toString().trim(), textPsw.getText().toString().trim()))
+        try{
+            (firebaseAuth.signInWithEmailAndPassword(textEmail.getText().toString().trim(), textPsw.getText().toString().trim()))
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -68,18 +70,22 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Intent i = new Intent(MainActivity.this, LoggedInActivity.class);
                             startActivity(i);
-                        } else {
-                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                            Log.e("ERROR", task.getException().toString());
                         }
                     }
                 });
+        }catch(IllegalArgumentException e){
+            progressDialog.dismiss();
+            Snackbar.make(this.findViewById(android.R.id.content), "Please enter your email and password!", Snackbar.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+            Log.e(getString(R.string.Error), e.toString());
+        }
     }
 
     public void Register(){
         ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this, getString(R.string.Please_Wait), getString(R.string.Processing), true);
 
-        (firebaseAuth.createUserWithEmailAndPassword(textEmail.getText().toString().trim(), textPsw.getText().toString().trim()))
+        try{
+            (firebaseAuth.createUserWithEmailAndPassword(textEmail.getText().toString().trim(), textPsw.getText().toString().trim()))
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -89,12 +95,15 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, R.string.AC_Successful, Toast.LENGTH_LONG).show();
                             Intent i = new Intent(MainActivity.this, MainActivity.class);
                             startActivity(i);
-                        } else {
-                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                            Log.e(getString(R.string.Error), task.getException().toString());
                         }
                     }
                 });
+        }catch(IllegalArgumentException e){
+            progressDialog.dismiss();
+            Snackbar.make(this.findViewById(android.R.id.content), "Please enter your email and password!", Snackbar.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+            Log.e(getString(R.string.Error), e.toString());
+        }
     }
 
 }
