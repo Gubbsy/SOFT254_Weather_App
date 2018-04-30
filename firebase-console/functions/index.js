82099cb019fcd1a6b6b0cc5f54f94b0f9a42a2d4
ThatGuyAndCo <32269506+ThatGuyAndCo@users.noreply.github.com>
2018-04-30 13:09:15 +0100

    Updated firebase function with correct test code

--- firebase-console/functions/index.js
+++ firebase-console/functions/index.js
@@ -1,31 +1,16 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
 
-exports.expireData = functions.database.ref("{weather-data = **}")
+exports.expireData = functions.database.ref("weather-info/{weather-data = **}")
 .onWrite((snap, context) => {
-  var ref = event.data.ref.parent; // reference to the items
-  var now = Date.now();
-  var cutoff = now - 2 * 60 * 60 * 1000;
-  var oldItemsQuery = ref.orderByChild('timestamp').endAt(cutoff);
-  return oldItemsQuery.once('value', function(snapshot) {
-    // create a map with all children that need to be removed
-    var updates = {};
+  var dataReference = snap.data.ref;
+  var currentTime = Date.now();
+  var expireDataAfter = now - 1000 * 60 * 60 * 24 * 3;
+  var oldData = ref.orderByChild('postTime').endAt(expireDataAfter);
+  return oldData.once('data', function(snapshot) {
+    var dataToBeDeleted = {};
     snapshot.forEach(function(child) {
-      updates[child.key] = null
+      dataToBeDeleted[child.key] = null
     });
-    // execute all updates in one go and return the result to end the function
-    return ref.update(updates);
+    return ref.update(dataToBeDeleted);
   });
-});
-
-exports.makeUppercase = functions.database.ref('/weather-info/{data}')
-    .onCreate((snapshot, context) => {
-      // Grab the current value of what was written to the Realtime Database.
-    //  const original = snapshot.val();
-      console.log("Pleeeeeeeeeeeeease woooooooooork!");
-      //const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      //return snapshot.ref.parent.child('uppercase').set(uppercase);
-    });
+})
